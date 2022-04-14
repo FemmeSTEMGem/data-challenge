c3069d70babcca9bf83c8d5b1600ece6e64b8619
FemmeSTEMGem <femmestemgem@gmail.com>
2022-04-14 15:53:24 -0400

    Day 2 challenge

---
+++ 02.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+# Day 2: Strings and the Print Function in Python¶
+# After using our data skills to browse through flights to Europe, we figured out it was best for Dot to fly out of Toronto. That’s no problem for them — they snagged a cheap flight to Toronto right away and rushed to pack their suitcase. Clothes, toiletries, a phone charger, and a book went neatly in, then Dot zipped it up and was out the door. On the way out, they put their key in the mailbox for their friend so that Python would get food and fresh water while Dot was away. On the flight over, they could barely contain their excitement. After a few hours, the plane began to descend, and the CN Tower came into view among the cityscape of skyscrapers. The first leg of their journey was already complete, and they were on their way to Europe.
+
+# After deboarding the plane, Dot stretched their stiff legs and stopped at a cafe counter to grab some hot coffee. They had a long flight ahead of them, but they didn’t mind — they were fulfilling a dream by travelling over to Europe. With joy in their heart, they strode over to the international wing of the airport to board the next plane. Luckily, their flights were timed so well that they didn’t even have much of a layover. Waiting to get on the massive, intercontinental plane, they prepared to show their credentials to the official. Their passport must be in perfect shape before they’re allowed on board — we can use data to help them.
+
+
+# Challenge
+# Modify the name on Dot’s passport so it can be recognized by passport control. Which combination of string functions will give us the right results?
+
+# name: Dot
+
+# Required format: DOT!
+
+
+
+name = '  Dot  '.strip().upper()
+print(name)
